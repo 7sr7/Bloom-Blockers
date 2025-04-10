1a123bb12977041c9e173787374b5e5811c01bd4
RoninSR7 <ronin.silvestre07@gmail.com>
2025-04-10 09:57:56 -0400

    gpio, adc configuring...

---
+++ ads1115_example.py
@@ -0,0 +1,32 @@
+import time
+import board
+import busio
+import adafruit_ads1x15.ads1115 as ADS
+from adafruit_ads1x15.analog_in import AnalogIn
+
+# Create the I2C bus
+i2c = busio.I2C(board.SCL, board.SDA)
+
+# Create the ADC object using the I2C bus
+ads = ADS.ADS1115(i2c)
+
+# Create single-ended input on channel 0
+chan0 = AnalogIn(ads, ADS.P0)
+chan1 = AnalogIn(ads, ADS.P1)
+chan2 = AnalogIn(ads, ADS.P2)
+chan3 = AnalogIn(ads, ADS.P3)
+
+print("{:>5}\t{:>5}".format("raw", "v"))
+
+try:
+    while True:
+        print("cahn0: {:>5}\t{:>5.3f}".format(chan0.value, chan0.voltage))
+        print("cahn1: {:>5}\t{:>5.3f}".format(chan1.value, chan1.voltage))
+        print("cahn2: {:>5}\t{:>5.3f}".format(chan2.value, chan2.voltage))
+        print("cahn3: {:>5}\t{:>5.3f}".format(chan3.value, chan3.voltage))
+        print("")
+        time.sleep(0.5)
+except KeyboardInterrupt:
+    pass
+
+
--- gpioTest.py
+++ gpioTest.py
@@ -1,16 +1,51 @@
-from gpiozero import LED
-from time import sleep
+from gpiozero import LED, DigitalOutputDevice
+import time
 
-# Use GPIO pin 17 (Pin 11 on the board)
-led = LED(17)
 
-print("🔁 Blinking LED on GPIO 17...")
+if False:
+    # Use GPIO pin 17 (Pin 11 on the board)
+    led = LED(17)
+
+    print("Blinking LED on GPIO 17...")
+
+    while True:
+        led.on()
+        print("LED ON")
+        sleep(1)
+        led.off()
+        print("LED OFF")
+        sleep(1)
+
+pin = DigitalOutputDevice(27)  # GPIO 27 (pin 13)
+
 
 while True:
-    led.on()
-    print("LED ON")
-    sleep(1)
-    led.off()
-    print("LED OFF")
-    sleep(1)
 
+    if False:
+        pin.on()   # Set HIGH
+        sleep(2)
+
+        pin.off()  # Set LOW
+    else:
+        pin.toggle()
+        sleep(2)
+
+
+
+
+
+if False:
+    motorPin = 9
+
+    def setup():
+        pinMode(motorPin, OUTPUT)
+        digitalWrite(motorPin, LOW)   # Ensure MOSFET is off at power‑up
+
+
+    def loop():
+        time.sleep(5)
+        digitalWrite(motorPin, HIGH);  # Turn the motor ON (via MOSFET)
+        time.sleep(5)
+
+        digitalWrite(motorPin, LOW);   # Turn the motor OFF
+        while (1) {}                   # Stop the program from restarting the cycle
